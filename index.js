7a60b5faaf56ce9d776e783b198d4b4b504ed416
Shiri Mund <shiribogo@gmail.com>
2018-03-18 20:29:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ function add(a.b) {
   return a+b 
 }
 
-function add(a.b) {
-  return a+b 
+function subtract(a.b) {
+  return a-b 
 }
 
-function add(a.b) {
-  return a+b 
+function multiply(a.b) {
+  return a*b 
 }
 
-function add(a.b) {
-  return a+b 
+function divide(a.b) {
+  return a/b 
 }
